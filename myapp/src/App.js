6653866d17d60e908d5cb1b995507fa47e7a2412
Burak5796 <burak.d5796@gmail.com>
2023-01-20 00:07:25 +0100

    formular

---
+++ myapp/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Registration</h1>
+      <form className="register" type="submit">
+        <label>Firstname:</label>
+        <input type="text"></input>
+        <label>Lastname:</label>
+        <input type="text"></input>
+        <label>Username:</label>
+        <input type="text" minlength="5"></input>
+        <label>E-Mail:</label>
+        <input type="email"></input>
+        <label>Password:</label>
+        <input type="password" name="pw"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+               title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>  
+        </input>
+        <label>Repeat Password:</label>
+        <input type="password" name="pw"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+               title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>  
+        </input>
+        <button id="submitbtn">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
