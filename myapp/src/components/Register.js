ef792a9a835d70d8bc10a8ef0a40da3ac962fcff
Burak5796 <burak.d5796@gmail.com>
2023-01-21 20:12:40 +0100

    Routes

---
+++ myapp/src/components/Register.js
@@ -0,0 +1,29 @@
+function Registration() {
+    return(
+       <>
+        <h1>Registration</h1>
+        <form className="register" type="submit">
+        <label>Firstname:</label>
+        <input type="text"></input>
+        <label>Lastname:</label>
+        <input type="text"></input>
+        <label>Username:</label>
+        <input type="text" minlength="5"></input>
+        <label>E-Mail:</label>
+        <input type="email"></input>
+        <label>Password:</label>
+        <input type="password" name="pw"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+               title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>  
+        </input>
+        <label>Repeat Password:</label>
+        <input type="password" name="pw"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+               title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>  
+        </input>
+        <button id="submitbtn">Submit</button>
+      </form>
+    </>
+    )
+}
+
+
+export default Registration;
